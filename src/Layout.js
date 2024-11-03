d4ad485de720c46e279ebf4d21b33d0e127002ea
Milos Solaja <milossolajaa@gmail.com>
2024-11-03 17:58:14 +0100

    Menu with buttons added

--- src/Layout.js
+++ src/Layout.js
@@ -1,24 +1,31 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import "./index.css"
 
+
 const Layout = () => {
+
+    const [activeSection, setActiveSection] = useState(null);
+
     const section1Ref = useRef(null);
     const section2Ref = useRef(null);
     const section3Ref = useRef(null);
   
-    const scrollToSection = (sectionRef) => {
+    const scrollToSection = (sectionRef, sectionNum) => {
       sectionRef.current.scrollIntoView({ behavior: "smooth" });
-    };
 
+      setActiveSection(sectionNum);
+    };
 
     return (
       <div className="layout">
         <aside className="sidebar">
           <div className="title">Milos Solaja</div>
           <div className="subtitle">AI and ML Researcher and Developer</div>
-          <button onClick={() => scrollToSection(section1Ref)}>Section 1</button>
-          <button onClick={() => scrollToSection(section2Ref)}>Section 2</button>
-          <button onClick={() => scrollToSection(section3Ref)}>Section 3</button>
+          <div class="menu">
+          <button onClick={() => scrollToSection(section1Ref, 1)} className={`menu-button ${activeSection === 1 ? "active" : ""}`}>Section 1</button>
+          <button onClick={() => scrollToSection(section2Ref, 2)} className={`menu-button ${activeSection === 2 ? "active" : ""}`}>Section 2</button>
+          <button onClick={() => scrollToSection(section3Ref, 3)} className={`menu-button ${activeSection === 3 ? "active" : ""}`}>Section 3</button>
+          </div>
         </aside>
         <main className="main-content">
             <section ref={section1Ref}>
