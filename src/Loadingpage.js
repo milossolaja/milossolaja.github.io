f9074f1f1b9eada7e91b50d6497bdeaafdcb7b03
Milos Solaja <milossolajaa@gmail.com>
2025-01-12 19:00:44 +0100

    logoanim moved

--- src/Loadingpage.js
+++ src/Loadingpage.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './loadingpage.css';
-import loadingAnim from './logoanim_background.mov';
 
 const Loadingpage = () => {
   return (
     <div className="loading-container">
         <video className="loading-video" autoPlay loop muted>
-        <source src={loadingAnim} type="video/mp4" />
+        <source src={'/logoanim_background.mov'} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
